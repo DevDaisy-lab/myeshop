137d51669c610f296495939022a52ef03ef822f1
Flutter <38566157+CodingFlutter@users.noreply.github.com>
2022-08-12 14:05:06 +0300

    extended widget removed

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
           debugShowCheckedModeBanner: false,
-          title: 'MyShop',
+          title: 'MyeShop',
           theme: ThemeData(
             textTheme: ThemeData.light().textTheme.copyWith(
                   headline1: TextStyle(
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -16,23 +16,21 @@ class ProductsGrid extends StatelessWidget {
     final products = showFavs ? productsData.favoriteItems : productsData.items;
     return Container(
       padding: EdgeInsets.only(left: 7, right: 7),
-      child: Expanded(
-        child: GridView.builder(
-          // padding: const EdgeInsets.all(2.0),
-          scrollDirection: Axis.vertical,
-          physics: ClampingScrollPhysics(),
-          itemCount: products.length,
-          itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
-            // builder: (c) => products[i],
-            value: products[i],
-            child: ProductItem(),
-          ),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 0.8,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-          ),
+      child: GridView.builder(
+        // padding: const EdgeInsets.all(2.0),
+        scrollDirection: Axis.vertical,
+        physics: ClampingScrollPhysics(),
+        itemCount: products.length,
+        itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+          // builder: (c) => products[i],
+          value: products[i],
+          child: ProductItem(),
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 0.8,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
         ),
       ),
     );
