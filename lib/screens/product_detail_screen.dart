9b3909a2da760a279970f5fe7ee5945ea6108581
Flutter <38566157+CodingFlutter@users.noreply.github.com>
2022-08-15 13:19:33 +0300

    update

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -6,16 +6,10 @@ import '../providers/product.dart';
 import '../providers/cart.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-  // final String title;
-  // final double price;
-
-  // ProductDetailScreen(this.title, this.price);
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-    // final product = Provider.of<Product>(context, listen: false);
-    // final cart = Provider.of<Cart>(context, listen: false);
     final productId =
         ModalRoute.of(context)?.settings.arguments as String; // is the id!
     final loadedProduct = Provider.of<Products>(
