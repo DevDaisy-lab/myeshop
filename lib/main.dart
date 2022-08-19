7ab82832770a9ab745b16f240d3b714bb73fad3d
Flutter <38566157+CodingFlutter@users.noreply.github.com>
2022-08-19 15:12:42 +0300

    works on Ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'package:provider/provider.dart';
 
@@ -15,8 +14,6 @@ import './providers/cart.dart';
 import './providers/orders.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
